196ad6b99b12d4d97f968b43f4a57bfbb00633c4
UlybinDA <97148056+UlybinDA@users.noreply.github.com>
2022-02-03 17:20:32 +0700

    Add files via upload

--- experiment.py
+++ experiment.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 from x_source import cidg_set_int
 from geometry import Vec3D
 from sample import BallAbsorber
+from  fabio_test_bruker100 import read_run
+
 
 x_beam = cidg_set_int()
 abs_ball = BallAbsorber
 shift_beam = np.array([0.,0.])#mm
 vec = Vec3D(-0.3, 0., 0.)#mm
 print(vec)
-rot_vec = vec.rot_euler('XYZ', [-54.74, 0., 45.])
-rot_vec = rot_vec.rot_euler('XYZ', [0., 0., 180.])
+rot_vec = vec.rot_euler('XYZ', [-54.74, 0., float(read_run(1,50).phi)])
+rot_vec = rot_vec.rot_euler('XYZ', [0., 0., 0.])
 rot_vec = rot_vec.rot_euler('XYZ', [0., 0., 0.])
 final_shift = -shift_beam + rot_vec.proj_yz
 final_shift = final_shift * 1000 #to microns
@@ -45,18 +47,33 @@ def set_experiment (x_beam, transm_mask, shift):
         raise ValueError('shift must be [y,z]')
     return b
 
+def compare_int (rns_rng, frm_rng, initial_shift, beam_shift): #shifts are mm
+    compare_table = open('compare_book.txt', "w")
+    compare_table.write('OM\tPHI\tDIF_SUM_I\n')
+    tran_mask = abs_ball().T_mask
+    for run in rns_rng:
+        for frm in frm_rng:
+            rot_vec = initial_shift.rot_euler('XYZ', [-54.74, 0., float(read_run(run, frm).phi)])
+            rot_vec = rot_vec.rot_euler('XYZ', [0., 0., float(read_run(run, frm).om)])
+            ball_yz_shift = rot_vec.proj_yz
+            shift = np.array(ball_yz_shift).tolist() #- beam_shift
+            shift[0] = shift[0] * 1000  # to microns
+            shift[1] = shift[1] * 1000
+            shift = [int(round(shift[0])), int(round(shift[1]))]
+            diff_int = read_run(run,frm).sum_I - np.sum(set_experiment(x_beam,tran_mask,shift), dtype=np.float32)
+            compare_table.write(f"{read_run(run,frm).om}\t{read_run(run,frm).phi}\t{diff_int}\n")
+            print(diff_int, read_run(run, frm).phi, shift)
+    print(f"run {run} is done")
+    raise compare_table.close()
+compare_int(range(1,6,1),range(1,360,1), Vec3D(-0.3 ,0. ,0.), [0,0])
 
 
-for phi in range(0,360,10):
-    vec = Vec3D(-0.3, 0., 0.)  # mm
 
-    rot_vec = vec.rot_euler('XYZ', [-54.74, 0., phi])
-    rot_vec = rot_vec.rot_euler('XYZ', [0., 0., 180.])
-    final_shift = final_shift * 1000  # to microns
-    final_shift = [int(round(final_shift[0])), int(round(final_shift[1]))]
-    set_experiment(x_beam,abs_ball().T_mask, final_shift)
-    print(final_shift)
-    fig, ax = plt.subplots()
-    cs = ax.pcolormesh(np.log(set_experiment(x_beam, abs_ball().T_mask, final_shift)))
-    cbar = fig.colorbar(cs)
-    plt.show()
+print(read_run(1,91).sum_I)
+print(np.sum(set_experiment(x_beam,abs_ball().T_mask,final_shift), dtype=np.float32))
+print(read_run(1,91).sum_I - np.sum(set_experiment(x_beam,abs_ball().T_mask,final_shift), dtype=np.float32))
+
+# fig, ax = plt.subplots()
+# cs = ax.pcolormesh(np.log(set_experiment(x_beam, abs_ball().T_mask, [0,0])))
+# cbar = fig.colorbar(cs)
+# plt.show()
